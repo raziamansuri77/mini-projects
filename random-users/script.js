f90ef14d6cf88077563c118a5f8fa53002dad67a
razia <raziamansuri77@gmail.com>
2025-04-19 10:40:44 +0530

    updated-random-user

--- random-users/script.js
+++ random-users/script.js
@@ -8,8 +8,8 @@ const getUser = async () => {
     userContainer.innerHTML = `
         <div   class="card flex flex-col items-center justify-center p-4  text-center  ">
           <img src="${user.picture.large}" alt="User Picture" class="user-image rounded-full" />
-          <h2 class="text-[18px] font-bold ">${user.name.first} ${user.name.last}</h2>
-          <p>${user.email}</p>
+          <h2 class="text-[18px] font-bold text-black font-semibold ">${user.name.first} ${user.name.last}</h2>
+          <p class="text-blue-400">${user.email}</p>
           <p>${user.location.city}, ${user.location.country}</p>
         </div>
       `;
